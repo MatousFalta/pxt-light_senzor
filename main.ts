4368ee8b9973ec12ffecb0b2fbc440b6d9f3188f
Matouš Falta <matous.falta.024@pslib.cz>
2025-01-27 09:55:31 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,38 @@
 let raceEnded: boolean = false
+Sensors.SetLightLevel()
 radio.setGroup(94)
 let start:number = 0
 let end:number = 1
+let isStarted:boolean = false
 const goalCompleted = (receivedNumber: number) => {
     if (receivedNumber === 1) {
         start = control.millis()
+        isStarted = true
+        basic.showString("s")
+        music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     }
 }
 radio.onReceivedNumber(goalCompleted)
-Sensors.OnLightDrop(function() {
-    
+input.onButtonPressed(Button.B, function(){
+    isStarted = false
+    Sensors.SetLightLevel()
 })
-
-let finalTime = end - start
-console.log(finalTime)
+Sensors.OnLightDrop(function() {
+    if(isStarted){
+        end = control.millis()
+        let finalTime:number = end - start
+        finalTime = finalTime * 100
+        Math.round(finalTime)
+        finalTime = finalTime / 100
+        console.log(finalTime)
+        radio.sendNumber(finalTime)
+        basic.showNumber(finalTime)
+        music.play(music.tonePlayable(Note.F, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    }
+})
+let finalTime:number = end - start
+input.onButtonPressed(Button.A, function() {
+    console.log(finalTime)
+    basic.showNumber(finalTime)
+})
 basic.pause(500)
-radio.sendNumber(finalTime)
